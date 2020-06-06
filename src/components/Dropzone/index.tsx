0a3b34158236a0a6d1dba7f0c7164472e3ea3d3d
FabioKeiitiOishi <fabio.oishi@outlook.com>
2020-06-06 12:38:54 -0300

    sending project

---
+++ src/components/Dropzone/index.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+import './styles.css';
+
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback(acceptedFile => {
+    const file = acceptedFile[0];
+
+    const fileUrl = URL.createObjectURL(file);
+
+    setSelectedFileUrl(fileUrl);
+    onFileUploaded(file);
+  }, [onFileUploaded]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/*'
+  });
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+        {
+          selectedFileUrl
+          ? <img src={selectedFileUrl} alt="Point Thumbnail" />
+          : (
+            <p>
+              <FiUpload />
+              Coloque sua imagem aqui ou clique aqui para selecionar a imagem
+            </p>
+          )
+        }
+    </div>
+  )
+}
+
+export default Dropzone;
